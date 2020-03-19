45057c0e8c6e266ca36624ef2e495b1b49eb8844
aashish <aashish@ubuntu>
2020-03-19 16:45:59 +0545

    Some modification and detailed summary of weather added

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const request = require('request');
 
 const URL = 'https://api.darksky.net/forecast/ab50cd36126b4928f245dedbfb477f85/37.8267,-122.4233';
 
-request(URL, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: URL, json: true }, (error, response) => {
+    // console.log(response.body.currently);
+    const currentTemperature = response.body.currently.temperature;
+    const rainProbability = response.body.currently.precipProbability;
+    const weatherSummary = response.body.daily.data[0].summary;
+
+    console.log('Current temperature is: ' + currentTemperature);
+    console.log('There is ' + rainProbability + '% chance of rain'); 
+    console.log(weatherSummary);
 })
