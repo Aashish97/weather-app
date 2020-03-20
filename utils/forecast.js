92df1865386ed935cf5333f4bd66139e7e7abba0
aashish <aashish@ubuntu>
2020-03-20 12:52:09 +0545

    Destructuring of the objects and using shorthands wherever possible

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,16 +4,16 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/ab50cd36126b4928f245dedbfb477f85/' + latitude + ',' + longitude;
-    request( {url, json: true}, (error, response) => {
+    request( {url, json: true}, (error, {body}) => {
         if (error){
             callback("Unable to connect with weather API!!!");
-        }else if (response.body.error){
+        }else if (body.error){
             callback("Unable to find the location");
         }else{
             callback(undefined, {
-                currentTemperature: response.body.currently.temperature,
-                rainProbability: response.body.currently.precipProbability,
-                weatherSummary: response.body.daily.data[0].summary
+                currentTemperature: body.currently.temperature,
+                rainProbability: body.currently.precipProbability,
+                weatherSummary: body.daily.data[0].summary
             }
             );
         }
