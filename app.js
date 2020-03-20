79b4f1798d66fd5fcf5e594b91bf4034be686322
aashish <aashish@ubuntu>
2020-03-20 12:15:46 +0545

    input address taken from user via command line

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const request = require('request');
+const yargs = require('yargs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('New York', (error, data) => {
-    error ? console.log(error) : 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
-        if(error){
-            return console.log(error);
-        }else{
-            console.log(data.location);
-            console.log('Summary: ',forecastData.weatherSummary);
-            console.log('Current Temperature: ',forecastData.currentTemperature);
-            console.log('Rain Probability: ',forecastData.rainProbability);
-        }
+const address = process.argv[2];
+
+if(!address){
+    console.log("Please provide address");
+}else{
+    geocode(address, (error, data) => {
+        error ? console.log(error) : 
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error){
+                return console.log(error);
+            }else{
+                console.log(data.location);
+                console.log('Summary: ',forecastData.weatherSummary);
+                console.log('Current Temperature: ',forecastData.currentTemperature);
+                console.log('Rain Probability: ',forecastData.rainProbability);
+            }
+        });
     });
-});
+}
+
+
+
 
