5d1c03bbf1ef662df05c5704eb2af07b45bfa461
aashish <aashish@ubuntu>
2020-03-20 11:46:07 +0545

    Chaining of callbacks to use the value of module geocode to forecast

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 geocode('New York', (error, data) => {
-    error ? console.log(error) : console.log(data);
-})
+    error ? console.log(error) : 
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error){
+            return console.log(error);
+        }else{
+            console.log(data.location);
+            console.log('Summary: ',forecastData.weatherSummary);
+            console.log('Current Temperature: ',forecastData.currentTemperature);
+            console.log('Rain Probability: ',forecastData.rainProbability);
+        }
+    });
+});
 
-forecast(-27.6841119, 152.893382, (error, data) => {
-    error ? console.log(error) : console.log(data);
-})
