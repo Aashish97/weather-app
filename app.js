c6bd774a7457f41bb562d4c5c637e007db5a2186
aashish <aashish@ubuntu>
2020-03-20 10:45:02 +0545

    Structure modification

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 const request = require('request');
 const geocode = require('./utils/geocode');
-
-const weatherURL = 'https://api.darksky.net/forecast/ab50cd36126b4928f245dedbfb477f85/37.8267,-122.4233';
-
-//weather api that provides all the required information about the weather
-
-request({ url: weatherURL, json: true }, (error, response) => {
-    if(error){
-        console.log("Unable to connect with weather API!!!");
-    }
-    else if (response.body.error) {
-        console.log("Unable to find location");
-    }
-    else{
-    const currentTemperature = response.body.currently.temperature;
-    const rainProbability = response.body.currently.precipProbability;
-    const weatherSummary = response.body.daily.data[0].summary;
-
-    console.log(weatherSummary);
-    console.log('Current temperature is: ' + currentTemperature);
-    console.log('There is ' + rainProbability + '% chance of rain'); 
-    }
-});
+const forecast = require('./utils/forecast');
 
 geocode('New York', (error, data) => {
-    if(error){
-        console.log(error);
-    }else{
-        console.log(data);
-    }
+    error ? console.log(error) : console.log(data);
+})
+
+forecast(-27.6841119, 152.893382, (error, data) => {
+    error ? console.log(error) : console.log(data);
 })
